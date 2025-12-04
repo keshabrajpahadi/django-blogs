a067b7f008ac17a768e83d0994130d11512da896
keshabrajpahadi <pahadikushal123@gmail.com>
2025-12-04 16:27:06 +0545

    post by category

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
-from blogs.models import Category
+from blogs.models import Category,Blog
 
 def home(request):
-    categories=Category.objects.all()
+   # categories=Category.objects.all()
+    
+    featured_posts=Blog.objects.filter(is_featured=True,status=1).order_by('-updated_at')
+    print(featured_posts)
+    posts=Blog.objects.filter(is_featured=True,status=1)
     context={
-        'categories':categories
+        #'categories':categories,
+        'featured_posts':featured_posts,
+        'posts':posts
     }
     return render(request,'home.html',context)
 
--- blogs/admin.py
+++ blogs/admin.py
@@ -5,7 +5,7 @@ from .models import Blog,Category
 class BlogAdmin(admin.ModelAdmin):
     prepopulated_fields={'slug':('title',)}
     list_display=('title','category','author','status','is_featured')
-    search_fields=('id','title','categogy__category_name','status')
+    search_fields=('id','title','category__category_name','status')
     list_editable=('is_featured',)
 
 admin.site.register(Category)
