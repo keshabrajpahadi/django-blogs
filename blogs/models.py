4ae9629c46ec79331f236c6fd10a8e4c7c4f2845
keshabrajpahadi <pahadikushal123@gmail.com>
2025-12-14 17:40:55 +0545

    comment feature add

--- blogs/models.py
+++ blogs/models.py
@@ -34,3 +34,15 @@ class Blog(models.Model):
 
     def __str__(self):
         return self.short_description
+    
+
+class Comment(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    blog=models.ForeignKey(Blog,on_delete=models.CASCADE)
+    comment=models.TextField(max_length=20000)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.comment
+
--- blogs/views.py
+++ blogs/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,get_object_or_404,redirect
-from .models import Blog,Category
+from django.http import HttpResponseRedirect
+from .models import Blog,Category,Comment
 from django.db.models import Q
 
 # Create your views here.
@@ -19,8 +20,19 @@ def posts_by_category(request,category_id):
 
 def blogs(request,slug):
     single_blog=get_object_or_404(Blog,slug=slug,status=1)
+    if request.method == "POST":
+        comment=Comment()
+        comment.user=request.user
+        comment.blog=single_blog
+        comment.comment= request.POST['comment']
+        comment.save()
+        return HttpResponseRedirect(request.path_info)
+    comments=Comment.objects.filter(blog=single_blog)
+    comment_count=Comment.objects.count()
     context={
-        'single_blog':single_blog
+        'single_blog':single_blog,
+        'comments':comments,
+        'comment_count':comment_count
     }
     return render(request,'blogs.html',context)
 
